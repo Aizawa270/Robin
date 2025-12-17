aeacac5e2f29a0ef6e5032b52fe3b1cca0da43b6
Aizawa270 <aizawa260@gmail.com>
2025-12-17 15:12:52 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 const { loadCommands, handleMessage } = require('./handlers/commandHandler');
+const { defaultPrefixless } = require('./config');
 
 const client = new Client({
   intents: [
@@ -12,8 +13,8 @@ const client = new Client({
 });
 
 // AFK + prefixless storage
-client.afk = new Map();        // userId -> { reason, since }
-client.prefixless = new Set(); // userIds who can use prefixless commands
+client.afk = new Map();                      // userId -> { reason, since }
+client.prefixless = new Set(defaultPrefixless || []); // always start with IDs from config
 
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
