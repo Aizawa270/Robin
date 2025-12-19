c3b8df87b4c305daddccc697b351fe1c4a20536e
Aizawa270 <aizawa260@gmail.com>
2025-12-19 19:53:54 +0600

    Update endgiveaway.js

--- commands/mod/endgiveaway.js
+++ commands/mod/endgiveaway.js
@@ -1,27 +1,27 @@
-const { PermissionsBitField } = require('discord.js');
 const Database = require('better-sqlite3');
 const path = require('path');
-const dbPath = path.join(__dirname, '../data/giveaways.sqlite');
-const db = new Database(dbPath);
+const { EmbedBuilder } = require('discord.js');
+
+const DATA_DIR = path.resolve(__dirname, '../data');
+const giveawayDB = new Database(path.join(DATA_DIR, 'giveaways.sqlite'));
 
 module.exports = {
   name: 'endgiveaway',
   aliases: ['egw'],
+  description: 'End a giveaway manually. Usage: $endgiveaway <channel>',
   category: 'utility',
   hidden: true,
-  usage: '$endgiveaway [#channel]',
   async execute(client, message, args) {
-    if (!message.member.permissions.has(PermissionsBitField.Flags.ManageGuild))
-      return message.reply('You need the **Manage Server** permission.');
+    let channel = message.channel;
+    if (args[0]) {
+      const ch = message.guild.channels.cache.get(args[0].replace(/[<#>]/g, ''));
+      if (ch) channel = ch;
+    }
 
-    let channel = message.mentions.channels.first() || message.channel;
-
-    // get last active giveaway in channel
-    const row = db.prepare('SELECT * FROM giveaways WHERE channel_id = ? ORDER BY end_timestamp ASC').get(channel.id);
+    const row = giveawayDB.prepare('SELECT * FROM giveaways WHERE channel_id = ? ORDER BY end_timestamp ASC LIMIT 1').get(channel.id);
     if (!row) return message.reply('No active giveaway in this channel.');
 
-    // call end manually
     require('./startgiveaway').endGiveaway(client, row.message_id);
-    message.reply('Giveaway ended.');
-  },
+    message.reply('Giveaway ended manually.');
+  }
 };
