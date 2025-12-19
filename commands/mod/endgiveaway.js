0ffbe77cc65dc49630438bad36e5794f79037972
Aizawa270 <aizawa260@gmail.com>
2025-12-19 21:42:23 +0600

    Update endgiveaway.js

--- commands/mod/endgiveaway.js
+++ commands/mod/endgiveaway.js
@@ -1,17 +1,26 @@
 module.exports = {
   name: 'endgiveaway',
   aliases: ['egw'],
-  hidden: true, // Not in help
+  hidden: true,
   async execute(client, message, args) {
-    const channel = message.mentions.channels.first() || message.channel;
-    const msgId = args[0]; // user must provide message ID of the giveaway
+    if (!message.member.permissions.has('Administrator')) {
+      return message.reply('You are not allowed to end giveaways.');
+    }
 
-    if (!msgId) return message.reply('Usage: $egw <giveawayMessageId> [channel]');
-    const g = client.giveawayDB.prepare('SELECT * FROM giveaways WHERE message_id = ?').get(msgId);
-    if (!g) return message.reply('No giveaway found with that message ID.');
+    const msgId = args[0];
+    if (!msgId) {
+      return message.reply('Usage: `$egw <giveawayMessageId>`');
+    }
 
-    // End giveaway manually
-    require('./startgiveaway').endGiveaway(client, msgId);
-    message.reply('Giveaway ended manually.');
+    const g = client.giveawayDB
+      .prepare('SELECT * FROM giveaways WHERE message_id = ?')
+      .get(msgId);
+
+    if (!g) {
+      return message.reply('No giveaway found with that message ID.');
+    }
+
+    await require('./startgiveaway').endGiveaway(client, msgId);
+    message.reply('Giveaway ended.');
   },
 };
