065d3688942a3be52bc94020913862b467a5a4e4
Aizawa270 <aizawa260@gmail.com>
2025-12-19 19:58:09 +0600

    Update endgiveaway.js

--- commands/mod/endgiveaway.js
+++ commands/mod/endgiveaway.js
@@ -1,27 +1,17 @@
-const Database = require('better-sqlite3');
-const path = require('path');
-const { EmbedBuilder } = require('discord.js');
-
-const DATA_DIR = path.resolve(__dirname, '../data');
-const giveawayDB = new Database(path.join(DATA_DIR, 'giveaways.sqlite'));
-
 module.exports = {
   name: 'endgiveaway',
   aliases: ['egw'],
-  description: 'End a giveaway manually. Usage: $endgiveaway <channel>',
-  category: 'utility',
-  hidden: true,
+  hidden: true, // Not in help
   async execute(client, message, args) {
-    let channel = message.channel;
-    if (args[0]) {
-      const ch = message.guild.channels.cache.get(args[0].replace(/[<#>]/g, ''));
-      if (ch) channel = ch;
-    }
+    const channel = message.mentions.channels.first() || message.channel;
+    const msgId = args[0]; // user must provide message ID of the giveaway
 
-    const row = giveawayDB.prepare('SELECT * FROM giveaways WHERE channel_id = ? ORDER BY end_timestamp ASC LIMIT 1').get(channel.id);
-    if (!row) return message.reply('No active giveaway in this channel.');
+    if (!msgId) return message.reply('Usage: $egw <giveawayMessageId> [channel]');
+    const g = client.giveawayDB.prepare('SELECT * FROM giveaways WHERE message_id = ?').get(msgId);
+    if (!g) return message.reply('No giveaway found with that message ID.');
 
-    require('./startgiveaway').endGiveaway(client, row.message_id);
+    // End giveaway manually
+    require('./startgiveaway').endGiveaway(client, msgId);
     message.reply('Giveaway ended manually.');
-  }
+  },
 };
