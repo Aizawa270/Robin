323a2881acfe8c78c7eccc813f313c40c2259c0d
Aizawa270 <aizawa260@gmail.com>
2025-12-28 03:28:01 +0600

    Update useitem.js

--- commands/economy/useitem.js
+++ commands/economy/useitem.js
@@ -17,13 +17,21 @@ module.exports = {
     const qty = items.getUserItemQty(message.author.id, item.id);
     if (!qty) return message.reply('You do not have this item.');
 
+    // Check faction restriction
+    const user = econ.getUser(message.author.id);
+    if (item.data?.factionOnly && !user.faction_id) {
+      return message.reply('This item can only be used by a faction member.');
+    }
+
     // remove item
     items.removeItem(message.author.id, item.id, 1);
 
-    // apply effect
-    let effectText = '';
-    const data = item.data ? JSON.parse(JSON.stringify(item.data)) : {};
+    // apply effect in memory
+    const data = item.data || {};
+    require('../../handlers/items').applyItemEffect(message.author.id, data);
 
+    // effect text
+    let effectText = '';
     if (data.jobBoost) effectText += `Job earnings boosted by ${data.jobBoost}% for next job.\n`;
     if (data.eventBoost) effectText += `Event rewards boosted by ${data.eventBoost}%.\n`;
     if (data.factionBoost) effectText += `Faction earnings boosted by ${data.factionBoost}%.\n`;
