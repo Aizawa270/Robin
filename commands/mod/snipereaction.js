4511926c3291c0f5655ffef8c795400681bb9aff
Aizawa270 <aizawa260@gmail.com>
2026-01-05 18:18:42 +0600

    Update snipereaction.js

--- commands/mod/snipereaction.js
+++ commands/mod/snipereaction.js
@@ -1,4 +1,4 @@
-const { EmbedBuilder } = require('discord.js');
+const { EmbedBuilder, PermissionsBitField } = require('discord.js');
 
 module.exports = {
   name: 'snipereaction',
@@ -8,6 +8,10 @@ module.exports = {
   hidden: true,
 
   async execute(client, message, args) {
+    if (!message.member.permissions.has(PermissionsBitField.Flags.ManageMessages)) {
+      return message.reply('You need the **Manage Messages** permission to use this command.');
+    }
+
     const snipes = client.reactionSnipes.get(message.channel.id) || [];
     if (!snipes.length) {
       return message.reply('No reaction snipes in this channel.');
