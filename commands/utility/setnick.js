b50c95c1db7ad6d4d4f7e10797a538a63f002d1f
Aizawa270 <aizawa260@gmail.com>
2026-08-03 13:09:52 +0600

    Update setnick.js

--- commands/utility/setnick.js
+++ commands/utility/setnick.js
@@ -1,46 +1,60 @@
 const { EmbedBuilder, PermissionFlagsBits } = require('discord.js');
 const { colors } = require('../../config');
+const { resolveMember } = require('../../handlers/universalHelper');
 
 module.exports = {
   name: 'setnick',
   description: 'Change the nickname of a user.',
   category: 'mod',
-  usage: '$setnick <@user|userID> <new nickname>',
+  usage: '$setnick <@user|userID|username> <new nickname>',
   aliases: ['nick'],
-  async execute(client, message, args) {
-    if (!message.guild) return message.reply('This command can only be used in a server.');
 
-    // Permission check
+  async execute(client, message, args) {
+    if (!message.guild) {
+      return message.reply('This command can only be used in a server.');
+    }
+
     if (!message.member.permissions.has(PermissionFlagsBits.ManageNicknames)) {
       return message.reply('You need the "Manage Nicknames" permission to use this command.');
     }
 
-    const targetUser = message.mentions.members.first() || (args[0] && await message.guild.members.fetch(args[0]).catch(() => null));
-    if (!targetUser) return message.reply('Please specify a valid user by mention or ID.');
+    if (!args.length) {
+      return message.reply(
+        `Usage: \`${message.prefix || '$'}setnick <@user|userID|username> <new nickname>\``
+      );
+    }
 
-    const newNick = args.slice(1).join(' ');
-    if (!newNick) return message.reply('Please provide a new nickname.');
+    const targetMember = await resolveMember(client, message, args[0]);
 
-    // Prevent changing bot's nickname if higher role
-    if (!targetUser.manageable) {
+    if (!targetMember) {
+      return message.reply('Please specify a valid user by mention, ID, or exact username.');
+    }
+
+    const newNick = args.slice(1).join(' ').trim();
+
+    if (!newNick) {
+      return message.reply('Please provide a new nickname.');
+    }
+
+    if (!targetMember.manageable) {
       return message.reply('I cannot change the nickname of this user (role hierarchy).');
     }
 
     try {
-      await targetUser.setNickname(newNick, `Changed by ${message.author.tag}`);
+      await targetMember.setNickname(newNick, `Changed by ${message.author.tag}`);
 
       const embed = new EmbedBuilder()
         .setColor(colors.roleinfo || '#00ffff')
         .setTitle('Nickname Changed')
-        .setDescription(`**${targetUser.user.tag}** has had their nickname changed to **${newNick}**.`)
-        .setThumbnail(targetUser.user.displayAvatarURL({ size: 1024 }))
+        .setDescription(`**${targetMember.user.tag}** has had their nickname changed to **${newNick}**.`)
+        .setThumbnail(targetMember.user.displayAvatarURL({ size: 1024 }))
         .setTimestamp()
         .setFooter({ text: `Changed by ${message.author.tag}` });
 
-      await message.reply({ embeds: [embed] });
+      return message.reply({ embeds: [embed] });
     } catch (err) {
       console.error('SetNick error:', err);
-      message.reply('There was an error trying to change that nickname.');
+      return message.reply('There was an error trying to change that nickname.');
     }
   },
 };
