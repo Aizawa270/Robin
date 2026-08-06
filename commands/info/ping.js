2336a2ec091e0b2c19fdb381cf8dea55051b84b8
Aizawa270 <aizawa260@gmail.com>
2026-08-06 11:02:50 +0600

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,25 +1,28 @@
 console.log('Loaded command: ping.js');
 
 const { EmbedBuilder } = require('discord.js');
-const { colors } = require('../../config');
 
 module.exports = {
   name: 'ping',
   description: 'Shows bot latency and API latency.',
   category: 'info',
   usage: '$ping',
+
   async execute(client, message) {
     const sent = await message.reply('Pinging...');
     const botPing = sent.createdTimestamp - message.createdTimestamp;
     const apiPing = Math.round(client.ws.ping);
 
-    const embed = new EmbedBuilder()
-      .setTitle('🏓 Pong!')
-      .setColor(colors.ping)
-      .addFields(
-        { name: 'Bot Ping', value: `${botPing}ms`, inline: true },
-        { name: 'API Ping', value: `${apiPing}ms`, inline: true },
-      );
+    const embed = typeof message.createEmbed === 'function'
+      ? message.createEmbed({
+          title: '🏓 Pong!',
+        })
+      : new EmbedBuilder().setColor('#5b0000').setTitle('🏓 Pong!');
+
+    embed.addFields(
+      { name: 'Bot Ping', value: `${botPing}ms`, inline: true },
+      { name: 'API Ping', value: `${apiPing}ms`, inline: true },
+    );
 
     if (message.guild?.iconURL()) {
       embed.setThumbnail(message.guild.iconURL({ size: 1024 }));
